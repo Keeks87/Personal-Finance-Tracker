01e26dfee4b314a326ea649be7e4ca45299f549c
Keenan <110526824+Keeks87@users.noreply.github.com>
2024-05-09 12:01:26 +0200

    Models and migrations

--- finance_project/finance_manager/models.py
+++ finance_project/finance_manager/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    pass
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Transaction(models.Model):
+    date = models.DateField()
+    description = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+class Budget(models.Model):
+    limit = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.OneToOneField(Category, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
